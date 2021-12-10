53fb773be50e4a615aea84e21d67d5cddc0ee6ea
yukihane <yukihane.feather@gmail.com>
2021-12-11 05:06:17 +0900

    create Application class

---
+++ com.packtpub.e4.headless.application/src/com/packtpub/e4/headless/application/Application.java
@@ -0,0 +1,18 @@
+package com.packtpub.e4.headless.application;
+
+import org.eclipse.equinox.app.IApplication;
+import org.eclipse.equinox.app.IApplicationContext;
+
+public class Application implements IApplication {
+
+    @Override
+    public Object start(final IApplicationContext context) throws Exception {
+        System.out.println("Headless Application");
+        return null;
+    }
+
+    @Override
+    public void stop() {
+    }
+
+}
