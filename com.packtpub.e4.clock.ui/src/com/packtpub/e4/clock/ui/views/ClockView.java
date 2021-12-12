aca2d3a1db33bd5794226d630af1e82d7494619c
yukihane <yukihane.feather@gmail.com>
2021-12-13 03:16:10 +0900

    ch2: Circular ClockView

--- com.packtpub.e4.clock.ui/src/com/packtpub/e4/clock/ui/views/ClockView.java
+++ com.packtpub.e4.clock.ui/src/com/packtpub/e4/clock/ui/views/ClockView.java
@@ -1,12 +1,22 @@
 package com.packtpub.e4.clock.ui.views;
 
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.PaintEvent;
+import org.eclipse.swt.events.PaintListener;
+import org.eclipse.swt.widgets.Canvas;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.ui.part.ViewPart;
 
 public class ClockView extends ViewPart {
-
     @Override
     public void createPartControl(final Composite parent) {
+        final Canvas clock = new Canvas(parent, SWT.NONE);
+        clock.addPaintListener(new PaintListener() {
+            @Override
+            public void paintControl(final PaintEvent e) {
+                e.gc.drawArc(e.x, e.y, e.width - 1, e.height - 1, 0, 360);
+            }
+        });
     }
 
     @Override
