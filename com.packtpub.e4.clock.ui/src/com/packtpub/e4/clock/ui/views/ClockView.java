f8329b85388c0ec0ae5e594230d20ea07bbf5951
yukihane <yukihane.feather@gmail.com>
2021-12-13 04:57:31 +0900

    ch2: Adding the TimeZone drop-down

--- com.packtpub.e4.clock.ui/src/com/packtpub/e4/clock/ui/views/ClockView.java
+++ com.packtpub.e4.clock.ui/src/com/packtpub/e4/clock/ui/views/ClockView.java
@@ -1,15 +1,19 @@
 package com.packtpub.e4.clock.ui.views;
 
 import com.packtpub.e4.clock.ui.ClockWidget;
+import java.time.ZoneId;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.graphics.Color;
 import org.eclipse.swt.graphics.RGB;
 import org.eclipse.swt.layout.RowData;
 import org.eclipse.swt.layout.RowLayout;
+import org.eclipse.swt.widgets.Combo;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.ui.part.ViewPart;
 
 public class ClockView extends ViewPart {
+    private Combo timeZones;
+
     @Override
     public void createPartControl(final Composite parent) {
         final Object[] objects = parent.getDisplay().getDeviceData().objects;
@@ -28,9 +32,15 @@ public class ClockView extends ViewPart {
         clock1.setLayoutData(new RowData(20, 20));
         clock2.setLayoutData(new RowData(50, 50));
         clock3.setLayoutData(new RowData(100, 100));
+        timeZones = new Combo(parent, SWT.DROP_DOWN);
+        timeZones.setVisibleItemCount(5);
+        for (final String zone : ZoneId.getAvailableZoneIds()) {
+            timeZones.add(zone);
+        }
     }
 
     @Override
     public void setFocus() {
+        timeZones.setFocus();
     }
 }
