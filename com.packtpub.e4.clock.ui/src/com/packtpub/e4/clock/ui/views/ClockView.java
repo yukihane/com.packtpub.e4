f3bf4d28c5505a458c0db0bba98e743dd7af615b
yukihane <yukihane.feather@gmail.com>
2021-12-13 03:30:30 +0900

    ch2: Using RowLayout

--- com.packtpub.e4.clock.ui/src/com/packtpub/e4/clock/ui/views/ClockView.java
+++ com.packtpub.e4.clock.ui/src/com/packtpub/e4/clock/ui/views/ClockView.java
@@ -2,12 +2,21 @@ package com.packtpub.e4.clock.ui.views;
 
 import com.packtpub.e4.clock.ui.ClockWidget;
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.layout.RowData;
+import org.eclipse.swt.layout.RowLayout;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.ui.part.ViewPart;
 
 public class ClockView extends ViewPart {
     @Override public void createPartControl(final Composite parent) {
-        new ClockWidget(parent, SWT.NONE);
+        final RowLayout layout = new RowLayout(SWT.HORIZONTAL);
+        parent.setLayout(layout);
+        final ClockWidget clock1 = new ClockWidget(parent, SWT.NONE);
+        final ClockWidget clock2 = new ClockWidget(parent, SWT.NONE);
+        final ClockWidget clock3 = new ClockWidget(parent, SWT.NONE);
+        clock1.setLayoutData(new RowData(20, 20));
+        clock2.setLayoutData(new RowData(50, 50));
+        clock3.setLayoutData(new RowData(100, 100));
     }
 
     @Override public void setFocus() {
