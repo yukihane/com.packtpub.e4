3c1292db024c660379aa42a83c0ccc33824c9dbf
yukihane <yukihane.feather@gmail.com>
2021-12-16 01:25:09 +0900

    ch3: TimeZoneTreeView with images

--- com.packtpub.e4.clock.ui/src/com/packtpub/e4/clock/ui/views/TimeZoneLabelProvider.java
+++ com.packtpub.e4.clock.ui/src/com/packtpub/e4/clock/ui/views/TimeZoneLabelProvider.java
@@ -2,12 +2,22 @@ package com.packtpub.e4.clock.ui.views;
 
 import java.time.ZoneId;
 import java.util.Map;
+import org.eclipse.jface.resource.ImageRegistry;
 import org.eclipse.jface.viewers.LabelProvider;
+import org.eclipse.swt.graphics.Image;
+import org.eclipse.ui.ISharedImages;
 
 public class TimeZoneLabelProvider extends LabelProvider {
+    private final ISharedImages images;
+    private final ImageRegistry ir;
 
-    @SuppressWarnings("rawtypes")
+    public TimeZoneLabelProvider(final ISharedImages images, final ImageRegistry ir) {
+        this.images = images;
+        this.ir = ir;
+    }
+
     @Override
+    @SuppressWarnings("rawtypes")
     public String getText(final Object element) {
         if (element instanceof Map) {
             return "Time Zones";
@@ -17,6 +27,25 @@ public class TimeZoneLabelProvider extends LabelProvider {
             return ((ZoneId) element).getId().split("/")[1];
         } else {
             return "Unknown type: " + element.getClass();
+        }
+    }
+
+    public Object[] getElements(final Object inputElement) {
+        if (inputElement instanceof Object[]) {
+            return (Object[]) inputElement;
+        } else {
+            return new Object[0];
+        }
+    }
+
+    @Override
+    public Image getImage(final Object element) {
+        if (element instanceof Map.Entry) {
+            return images.getImage(ISharedImages.IMG_OBJ_FOLDER);
+        } else if (element instanceof ZoneId) {
+            return ir.get("sample");
+        } else {
+            return super.getImage(element);
         }
     }
 }
--- com.packtpub.e4.clock.ui/src/com/packtpub/e4/clock/ui/views/TimeZoneTreeView.java
+++ com.packtpub.e4.clock.ui/src/com/packtpub/e4/clock/ui/views/TimeZoneTreeView.java
@@ -1,19 +1,34 @@
 package com.packtpub.e4.clock.ui.views;
 
 import com.packtpub.e4.clock.ui.internal.TimeZoneComparator;
+import java.net.URL;
 import javax.annotation.PostConstruct;
+import javax.inject.Inject;
 import org.eclipse.e4.ui.di.Focus;
+import org.eclipse.jface.resource.ImageDescriptor;
+import org.eclipse.jface.resource.ImageRegistry;
+import org.eclipse.jface.resource.JFaceResources;
+import org.eclipse.jface.resource.LocalResourceManager;
+import org.eclipse.jface.resource.ResourceManager;
 import org.eclipse.jface.viewers.TreeViewer;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.widgets.Composite;
+import org.eclipse.ui.ISharedImages;
 
 public class TimeZoneTreeView {
     private TreeViewer treeViewer;
+    @Inject
+    private ISharedImages images;
 
     @PostConstruct
     public void create(final Composite parent) {
+        final ResourceManager rm = JFaceResources.getResources();
+        final LocalResourceManager lrm = new LocalResourceManager(rm, parent);
+        final ImageRegistry ir = new ImageRegistry(lrm);
+        final URL sample = getClass().getResource("/icons/sample.png");
+        ir.put("sample", ImageDescriptor.createFromURL(sample));
         treeViewer = new TreeViewer(parent, SWT.H_SCROLL | SWT.V_SCROLL | SWT.MULTI);
-        treeViewer.setLabelProvider(new TimeZoneLabelProvider());
+        treeViewer.setLabelProvider(new TimeZoneLabelProvider(images, ir));
         treeViewer.setContentProvider(new TimeZoneContentProvider());
         treeViewer.setInput(new Object[] { TimeZoneComparator.getTimeZones() });
     }
