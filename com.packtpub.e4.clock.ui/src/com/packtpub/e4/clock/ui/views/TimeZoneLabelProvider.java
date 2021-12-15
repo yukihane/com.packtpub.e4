30cf836325da5588a50064d4ce8a979af81c14de
yukihane <yukihane.feather@gmail.com>
2021-12-16 01:38:38 +0900

    TimeZoneTreeView with fonts

--- com.packtpub.e4.clock.ui/src/com/packtpub/e4/clock/ui/views/TimeZoneLabelProvider.java
+++ com.packtpub.e4.clock.ui/src/com/packtpub/e4/clock/ui/views/TimeZoneLabelProvider.java
@@ -1,19 +1,29 @@
 package com.packtpub.e4.clock.ui.views;
 
 import java.time.ZoneId;
+import java.time.ZoneOffset;
+import java.time.ZonedDateTime;
 import java.util.Map;
+import org.eclipse.jface.resource.FontRegistry;
 import org.eclipse.jface.resource.ImageRegistry;
+import org.eclipse.jface.resource.JFaceResources;
+import org.eclipse.jface.viewers.DelegatingStyledCellLabelProvider.IStyledLabelProvider;
+import org.eclipse.jface.viewers.IFontProvider;
 import org.eclipse.jface.viewers.LabelProvider;
+import org.eclipse.jface.viewers.StyledString;
+import org.eclipse.swt.graphics.Font;
 import org.eclipse.swt.graphics.Image;
 import org.eclipse.ui.ISharedImages;
 
-public class TimeZoneLabelProvider extends LabelProvider {
+public class TimeZoneLabelProvider extends LabelProvider implements IStyledLabelProvider, IFontProvider {
     private final ISharedImages images;
     private final ImageRegistry ir;
+    private final FontRegistry fr;
 
-    public TimeZoneLabelProvider(final ISharedImages images, final ImageRegistry ir) {
+    public TimeZoneLabelProvider(final ISharedImages images, final ImageRegistry ir, final FontRegistry fr) {
         this.images = images;
         this.ir = ir;
+        this.fr = fr;
     }
 
     @Override
@@ -47,5 +57,23 @@ public class TimeZoneLabelProvider extends LabelProvider {
         } else {
             return super.getImage(element);
         }
+    }
+
+    @Override
+    public StyledString getStyledText(final Object element) {
+        final String text = getText(element);
+        final StyledString styledString = new StyledString(text);
+        if (element instanceof ZoneId) {
+            final ZoneId zone = (ZoneId) element;
+            final ZoneOffset offset = ZonedDateTime.now(zone).getOffset();
+            styledString.append(" (" + offset + ")", StyledString.DECORATIONS_STYLER);
+        }
+        return styledString;
+    }
+
+    @Override
+    public Font getFont(final Object element) {
+        final Font italic = fr.getItalic(JFaceResources.DEFAULT_FONT);
+        return italic;
     }
 }
--- com.packtpub.e4.clock.ui/src/com/packtpub/e4/clock/ui/views/TimeZoneTreeView.java
+++ com.packtpub.e4.clock.ui/src/com/packtpub/e4/clock/ui/views/TimeZoneTreeView.java
@@ -5,11 +5,13 @@ import java.net.URL;
 import javax.annotation.PostConstruct;
 import javax.inject.Inject;
 import org.eclipse.e4.ui.di.Focus;
+import org.eclipse.jface.resource.FontRegistry;
 import org.eclipse.jface.resource.ImageDescriptor;
 import org.eclipse.jface.resource.ImageRegistry;
 import org.eclipse.jface.resource.JFaceResources;
 import org.eclipse.jface.resource.LocalResourceManager;
 import org.eclipse.jface.resource.ResourceManager;
+import org.eclipse.jface.viewers.DelegatingStyledCellLabelProvider;
 import org.eclipse.jface.viewers.TreeViewer;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.widgets.Composite;
@@ -25,10 +27,11 @@ public class TimeZoneTreeView {
         final ResourceManager rm = JFaceResources.getResources();
         final LocalResourceManager lrm = new LocalResourceManager(rm, parent);
         final ImageRegistry ir = new ImageRegistry(lrm);
+        final FontRegistry fr = new FontRegistry();
         final URL sample = getClass().getResource("/icons/sample.png");
         ir.put("sample", ImageDescriptor.createFromURL(sample));
         treeViewer = new TreeViewer(parent, SWT.H_SCROLL | SWT.V_SCROLL | SWT.MULTI);
-        treeViewer.setLabelProvider(new TimeZoneLabelProvider(images, ir));
+        treeViewer.setLabelProvider(new DelegatingStyledCellLabelProvider(new TimeZoneLabelProvider(images, ir, fr)));
         treeViewer.setContentProvider(new TimeZoneContentProvider());
         treeViewer.setInput(new Object[] { TimeZoneComparator.getTimeZones() });
     }
