3b7cc99c8a85986b0a4b426ee6a59b8f98d56764
yukihane <yukihane.feather@gmail.com>
2021-12-16 04:25:57 +0900

    ch3: Synchronizing selection

--- com.packtpub.e4.clock.ui/src/com/packtpub/e4/clock/ui/views/TimeZoneTableView.java
+++ com.packtpub.e4.clock.ui/src/com/packtpub/e4/clock/ui/views/TimeZoneTableView.java
@@ -6,8 +6,13 @@ import com.packtpub.e4.clock.ui.internal.TimeZoneOffsetColumn;
 import com.packtpub.e4.clock.ui.internal.TimeZoneSummerTimeColumn;
 import java.time.ZoneId;
 import javax.annotation.PostConstruct;
+import javax.inject.Inject;
+import javax.inject.Named;
+import org.eclipse.e4.core.di.annotations.Optional;
 import org.eclipse.e4.ui.di.Focus;
+import org.eclipse.e4.ui.services.IServiceConstants;
 import org.eclipse.jface.viewers.ArrayContentProvider;
+import org.eclipse.jface.viewers.StructuredSelection;
 import org.eclipse.jface.viewers.TableViewer;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.widgets.Composite;
@@ -30,5 +35,14 @@ public class TimeZoneTableView {
     @Focus
     public void focus() {
         tableViewer.getControl().setFocus();
+    }
+
+    @Inject
+    @Optional
+    public void setTimeZone(@Named(IServiceConstants.ACTIVE_SELECTION) final ZoneId timeZone) {
+        if (timeZone != null && tableViewer != null) {
+            tableViewer.setSelection(new StructuredSelection(timeZone));
+            tableViewer.reveal(timeZone);
+        }
     }
 }
