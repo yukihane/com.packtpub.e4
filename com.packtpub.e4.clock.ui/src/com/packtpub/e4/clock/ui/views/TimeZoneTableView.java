5301b4d1526f627cdead011eb6312c89f6b0bf21
yukihane <yukihane.feather@gmail.com>
2021-12-16 04:05:57 +0900

    ch3: Creating TimeZoneTableView

---
+++ com.packtpub.e4.clock.ui/src/com/packtpub/e4/clock/ui/views/TimeZoneTableView.java
@@ -0,0 +1,26 @@
+package com.packtpub.e4.clock.ui.views;
+
+import java.time.ZoneId;
+import javax.annotation.PostConstruct;
+import org.eclipse.e4.ui.di.Focus;
+import org.eclipse.jface.viewers.ArrayContentProvider;
+import org.eclipse.jface.viewers.TableViewer;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.widgets.Composite;
+
+public class TimeZoneTableView {
+    private TableViewer tableViewer;
+
+    @PostConstruct
+    public void create(final Composite parent) {
+        tableViewer = new TableViewer(parent, SWT.H_SCROLL | SWT.V_SCROLL);
+        tableViewer.getTable().setHeaderVisible(true);
+        tableViewer.setContentProvider(ArrayContentProvider.getInstance());
+        tableViewer.setInput(ZoneId.getAvailableZoneIds());
+    }
+
+    @Focus
+    public void focus() {
+        tableViewer.getControl().setFocus();
+    }
+}
